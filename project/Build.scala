f2019a35ea5b2d35cd18874e1806e1345089b196
fabio <pasquale.gatto@me.com>
2014-08-13 20:21:29 +0100

    1st commit

---
+++ project/Build.scala
@@ -0,0 +1,41 @@
+import sbt._
+import Keys._
+
+object HelloWorldInAmazon extends Build {
+  import Dependency._
+  import Resolvers._
+  import BuildSettings._
+  import scala.collection._
+
+  lazy val root = Project(id = "HelloWorldEmbededJetty", base = file("."), settings = standardBuildSettings ++ Seq(
+    resolvers := Seq(jettyRepo, resolver, Classpaths.typesafeResolver),
+    libraryDependencies ++= jetty ++ scalaTest
+  ))
+}
+
+object BuildSettings {
+  val standardBuildSettings: Seq[sbt.Project.Setting[_]] = Defaults.defaultSettings ++ Seq[Setting[_]](
+      organization := "HelloWorldEmbededJetty",     //TODO these 3 lines are ignored here and picked from build.sbt instead
+      version := "1.0",
+      scalaVersion := "2.10.4"
+  )
+}
+
+object Resolvers {
+  val jettyRepo = "jetty repo" at "http://siasia.github.com/maven2"
+  val resolver = "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+}
+
+
+object Dependency {
+  private val jettyVersion = "8.1.7.v20120910"
+  private val scalaTestVersion = "2.2.1"
+  //  val seleniumVersion = "2.6.0"
+
+  val jetty = Seq (
+      "org.eclipse.jetty" % "jetty-server" % jettyVersion,
+      "org.eclipse.jetty" % "jetty-webapp" % jettyVersion
+    )
+
+  val scalaTest = Seq("org.scalatest" % "scalatest_2.11" % scalaTestVersion  % "test")
+}
---
+++ src/main/scala/webserver/WebServer.scala
@@ -0,0 +1,53 @@
+package webserver
+
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.nio.SelectChannelConnector
+import org.eclipse.jetty.webapp.WebAppContext
+import java.io.File
+
+object WebServer extends App {
+
+  val serverPort = 8080
+  val productionMode = "production"
+  private val server = createServer
+  private val context = createContext
+  server.setHandler(context)
+
+
+  private def createServer = {
+    val server = new Server
+    val selectChannelConnector = new SelectChannelConnector
+    selectChannelConnector.setPort(serverPort)
+    server.setConnectors(Array(selectChannelConnector))
+    server
+  }
+
+  def startServer = {
+    try {
+      println(">>> STARTING EMBEDDED JETTY SERVER")
+      server.start()
+      println(">>> JETTY SERVER STARTED")
+      while (!server.isRunning) Thread.sleep(100)
+    } catch {
+      case exception: Exception => {
+        println("FAILED TO START JETTY SERVER")
+        exception.printStackTrace()
+        throw exception
+      }
+    }
+  }
+
+
+  private def createContext = {
+    val context = new WebAppContext()
+    context.setServer(server)
+    context.setContextPath("/")
+    if(new File("src/main/webapp").exists())
+      context.setWar("src/main/webapp")
+    else
+      context.setWar(getClass.getClassLoader.getResource("webapp").toExternalForm)
+    context
+  }
+
+   startServer
+ }
