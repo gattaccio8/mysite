d76de9bf46efe648cfc2ac2c78d376a8b41caf86
fabio <pasquale.gatto@me.com>
2014-09-20 12:40:34 +0100

    start/stop app function

--- webappDeploy.sh
+++ webappDeploy.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-nohup java -cp $(echo *.jar | tr ' ' ':') webserver.WebServer > app.log 2>&1 &
+function start() {
+    nohup java -cp $(echo *.jar | tr ' ' ':') webserver.WebServer > app.log 2>&1 &
+    }
+
+function stop() {
+    PID=`currentpid`
+    echo "Killing PID ${PID}...."
+    kill $PID
+}
+
+
+function currentpid() {
+    ps -ef | awk '/[w]ebserver/{print $2}'
+}
+
+case "$1" in
+    'start')
+    echo "Starting application"
+    start
+;;
+    'stop')
+    echo "Stopping application"
+    stop
+;;
+esac
+
