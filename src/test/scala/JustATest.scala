9b004daba82956f7a718a9b43023e2e9243dc1bf
fabio <pasquale.gatto@me.com>
2014-10-11 22:14:53 +0100

    share WebServer in WebSpecification for every test

--- src/test/scala/JustATest.scala
+++ src/test/scala/JustATest.scala
@@ -2,18 +2,16 @@ import org.scalatest.selenium.HtmlUnit
 import org.scalatest.{ShouldMatchers, FlatSpec}
 import webserver.WebServer
 
-class JustATest extends FlatSpec with ShouldMatchers with HtmlUnit {
+class JustATest extends FlatSpec with ShouldMatchers with HtmlUnit with WebSpecification {
 
   val host = "http://localhost:8080/"
 
   "The app home page" should "have the correct title" in {
-      WebServer
-      go to (host + "index.html")
-      pageTitle should be ("mysite title")
+    go to (host + "index.html")
+    pageTitle should be ("mysite title")
   }
 
   "it" should "have a h1 msg" in {
-    WebServer
     go to (host + "index.html")
     id("helloText").element.isDisplayed
   }
---
+++ src/test/scala/WebSpecification.scala
@@ -0,0 +1,5 @@
+import webserver.WebServer
+
+trait WebSpecification {
+  WebServer
+}
