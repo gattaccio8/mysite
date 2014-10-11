db7105c50d05e098fd7ec4d02d8bee130724dd45
fabio <pasquale.gatto@me.com>
2014-10-11 22:09:41 +0100

    just an other test

--- src/test/scala/JustATest.scala
+++ src/test/scala/JustATest.scala
@@ -6,10 +6,15 @@ class JustATest extends FlatSpec with ShouldMatchers with HtmlUnit {
 
   val host = "http://localhost:8080/"
 
-  "The app home page" should
-    "have the correct title" in {
+  "The app home page" should "have the correct title" in {
       WebServer
       go to (host + "index.html")
       pageTitle should be ("mysite title")
+  }
+
+  "it" should "have a h1 msg" in {
+    WebServer
+    go to (host + "index.html")
+    id("helloText").element.isDisplayed
   }
 }
