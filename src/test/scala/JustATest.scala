7e806f86eef44fbcab121732de18081b68f37b9a
pasqualegatto <pasquale.gatto@me.com>
2015-02-16 19:14:02 +0000

    adding bootstrap

--- src/test/scala/JustATest.scala
+++ src/test/scala/JustATest.scala
@@ -1,17 +1,20 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.scalatest.selenium.HtmlUnit
 import org.scalatest.{FunSuite, ShouldMatchers}
 
 class JustATest extends FunSuite with ShouldMatchers with HtmlUnit with WebSpecification {
 
   val host = "http://localhost:8080/"
+  val driver = new HtmlUnitDriver(BrowserVersion.FIREFOX_17)
 
   test("The app home page have the correct title") {
-    go to (host + "index.html")
-    assert(pageTitle === "mysite title")
+    driver.navigate().to(host + "index.html")
+    assert(driver.getTitle === "mysite title")
   }
 
   test("have a h1 msg") {
-    go to (host + "index.html")
-    assert(id("helloText").element.isDisplayed)
+    driver.navigate().to(host + "index.html")
+    assert(driver.findElementById("helloText").getText === "Hello World!")
   }
 }
